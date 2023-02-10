89e9c9c293c621f3256bf0521844be5eee272276
Jazmin Ivy <princessivy07@yahoo.com>
2023-02-10 15:20:33 -0600

    Updated day_trip.py file

--- day_trip.py
+++ day_trip.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+import random
+
+# Create Destination, Restaurant, Entertainment, and Transportation Lists
+destinations = ["Dallas, TX", "Orlando, FL", "Las Vegas, NV", "Las Angeles, CA", "Maui, HI"]
+restaurants = []
+entertainment = []
+transportation = []
+
+selected_destination = random.randrange(len(destinations))
+print(f"We have selected {destinations[selected_destination]} for your destination! Are you satisfied with this location?")
+confirm_choice = input("Please enter Y/N to confirm this destination.")
