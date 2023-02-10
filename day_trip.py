fc1528059b5bc92e4d7b1924234670a6a2d53834
Jazmin Ivy <princessivy07@yahoo.com>
2023-02-10 16:58:07 -0600

    Updated day_trip.py file

--- day_trip.py
+++ day_trip.py
@@ -30,7 +30,39 @@ def selected_transportation():
 
 selected_transportation = random.randrange(len(transportation))
 
-print(f"For your Day Trip we have selected {destinations(selected_destination)} for your destination,{restaurants(selected_restaurant)} for your restaurant, {entertainment(selected_entertainment)} for your entertainment, and {transportation(selected_transportation)} for your transportation.")
-
+print(f"For your Day Trip we have selected {selected_destination}")
+# print(f"For your Day Trip we have selected {destinations[selected_destination]} for your destination,{restaurants(selected_restaurant)} for your restaurant, {entertainment(selected_entertainment)} for your entertainment, and {transportation(selected_transportation)} for your transportation.")
+# Easy Way
 confirm_choice = input("Please enter Y/N to confirm this destination. ")
+
+# # Or Hard Way
+# def edit_selection():
+#     print("Which option would you like to change? Destination, Restaurant, Entertainment, Transportation")
+#     change_response = input("""
+#     Please enter one of the following options;
+#     a. Destination
+#     b. Restaurant
+#     c. Entertainment
+#     d. Transportation
+#     """)
+#     if change_response == "a":
+#         edit_destination()
+#     elif change_response == "b":
+#         edit_restaurant()
+#     elif change_response == "c":
+#         edit_entertainment()
+#     elif change_response == "d":
+#         edit_transportation()
+#     else:
+#         get_summary()
+
+# def edit_destination():
+#     get_destination = selected_destination()
+#     new_dest_response = input(f"Would you rather visit {get_destination} instead? Y/N ")
+#     if new_dest_response == "Y":
+#         print("Ok!")
+#         get_summary()
+#     elif new_dest_response == "N":
+#         edit_destination
+
 
