8efbd82a3779ad8532226f4b6a4d0dd019874fcd
Jazmin Ivy <princessivy07@yahoo.com>
2023-02-10 16:25:40 -0600

    Updated day_trip.py file

--- day_trip.py
+++ day_trip.py
@@ -10,21 +10,27 @@ def selected_destination():
     dest_rand_result = random.choice(destinations)
     return dest_rand_result
 
+selected_destination = random.randrange(len(destinations))
+
 def selected_restaurant():
     rest_rand_result = random.choice(restaurants)
     return rest_rand_result
+
+selected_restaurant = random.randrange(len(restaurants))
 
 def selected_entertainment():
     enter_rand_result = random.choice(entertainment)
     return enter_rand_result
 
+selected_entertainment = random.randrange(len(entertainment))
+
 def selected_transportation():
     trans_rand_result = random.choice(transportation)
     return trans_rand_result
 
-# selected_destination = random.randrange(len(destinations))
+selected_transportation = random.randrange(len(transportation))
 
-# print(f"For your Day Trip we have selected {destinations(selected_destination)} for your destination,{restaurants(selected_restaurant)} for your restaurant, {entertainment(selected_entertainment)} for your entertainment, and {transportation(selected_transportation)} for your transportation.")
+print(f"For your Day Trip we have selected {destinations(selected_destination)} for your destination,{restaurants(selected_restaurant)} for your restaurant, {entertainment(selected_entertainment)} for your entertainment, and {transportation(selected_transportation)} for your transportation.")
 
-# confirm_choice = input("Please enter Y/N to confirm this destination. ")
+confirm_choice = input("Please enter Y/N to confirm this destination. ")
 
