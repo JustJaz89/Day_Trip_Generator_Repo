53c89eb1248e22b279e31444c376c84ba1e9f267
Jazmin Ivy <princessivy07@yahoo.com>
2023-02-10 15:34:17 -0600

    Updated day_trip.py file

--- day_trip.py
+++ day_trip.py
@@ -2,9 +2,9 @@ import random
 
 # Create Destination, Restaurant, Entertainment, and Transportation Lists
 destinations = ["Dallas, TX", "Orlando, FL", "Las Vegas, NV", "Las Angeles, CA", "Maui, HI"]
-restaurants = []
-entertainment = []
-transportation = []
+restaurants = ["Benihana", "Red Lobster", "Olive Garden", "Texas de Brazil", "Halo Hawaiian BBQ & Poke Bar"]
+entertainment = ["Live Music", "Escape Room", "Indoor Go Karts", "Bowling", "Indoor Climbing"]
+transportation = ["Party Bus", "Rental Car", "Limousine", "Walking", "Rideshare"]
 
 selected_destination = random.randrange(len(destinations))
 print(f"We have selected {destinations[selected_destination]} for your destination! Are you satisfied with this location?")
