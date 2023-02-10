d06c666755a3e9a0be985813011847d5631f07d8
Jazmin Ivy <princessivy07@yahoo.com>
2023-02-10 15:46:14 -0600

    Updated day_trip.py file

--- day_trip.py
+++ day_trip.py
@@ -6,6 +6,26 @@ restaurants = ["Benihana", "Red Lobster", "Olive Garden", "Texas de Brazil", "Ha
 entertainment = ["Live Music", "Escape Room", "Indoor Go Karts", "Bowling", "Indoor Climbing"]
 transportation = ["Party Bus", "Rental Car", "Limousine", "Walking", "Rideshare"]
 
-selected_destination = random.randrange(len(destinations))
-print(f"We have selected {destinations[selected_destination]} for your destination! Are you satisfied with this location?")
-confirm_choice = input("Please enter Y/N to confirm this destination.")
+def selected_destination():
+    dest_rand_result = random.choice(destinations)
+    return dest_rand_result
+
+def selected_restaurant():
+    rest_rand_result = random.choice(restaurants)
+    return rest_rand_result
+
+def selected_entertainment():
+    enter_rand_result = random.choice(entertainment)
+    return enter_rand_result
+
+def selected_transportation():
+    trans_rand_result = random.choice(transportation)
+    return trans_rand_result
+
+
+
+
+
+# selected_destination = random.randrange(len(destinations))
+# print(f"We have selected {destinations[selected_destination]} for your destination! Are you satisfied with this location?")
+# confirm_choice = input("Please enter Y/N to confirm this destination.")
