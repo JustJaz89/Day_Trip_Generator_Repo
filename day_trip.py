7f6617896b7a154d327c8564ddc7118f0f2f5e80
Jazmin Ivy <princessivy07@yahoo.com>
2023-02-12 23:35:14 -0600

    Updated day_trip.py file and day_trip_ideas.py file

--- day_trip.py
+++ day_trip.py
@@ -32,44 +32,94 @@ b. {selected_restaurant}
 c. {selected_entertainment}
 d. {selected_transportation}""")
 print(day_trip)
-confirm_choice = input("Please enter Y/N to confirm this destination. ")
 
-def new_destination(confirm_choice):
-    while confirm_choice != "Y":
-        selected_destination = random.choice(destinations)
-        confirm_choice = input(f"Would you rather visit {selected_destination} instead? ")
-    else:
-        print(f"You have selected {selected_destination} as the destination for your trip. ")
+def get_dest():
+    destination_choice = input(f"Does this location, {selected_destination} interest you? Y/N: ")
+    if destination_choice == "Y":
+        print("Ok!")
+        get_rest()
+    elif destination_choice == "N":
+        print("Let's try again.")
+        get_dest()
 
-new_destination(confirm_choice)
+def get_rest():
+    get_restaurant = selected_restaurant()
+    restaurant_choice = input(f"Does this restaurant, {selected_restaurant} interest you? Y/N: ")
+    if restaurant_choice == "Y":
+        print("Great!")
+        get_enter()
+    elif restaurant_choice == "Y":
+        print("Let's try something else.")
+        get_rest()
 
-def new_restaurant(confirm_choice):
-    while confirm_choice != "Y":
-        selected_restaurant = random.choice(restaurants)
-        confirm_choice = input(f"Would you rather visit {selected_restaurant} instead? ")
-    else:
-        print(f"You have selected {selected_restaurant} as the destination for your trip. ")
+def get_enter():
+    get_entertainment = selected_entertainment()
+    entertainment_choice = input(f"Does this type of entertainment, {selected_entertainment} interest you? Y/N: ")
+    if entertainment_choice == "Y":
+        print("Awesome!")
+        get_trans()
+    elif entertainment_choice == "N":
+        print("Let's try again.")
+        get_enter()
 
-new_restaurant(confirm_choice)
+def get_trans():
+    get_transportation = selected_transportation()
+    transportation_choice = input(f"Does this type of transportation, {selected_transportation} interest you? Y/N: ")
+    if transportation_choice == "Y":
+        print("Amazing!")
+        get_summary()
+    elif transportation_choice == "N":
+        print("Let's try something else.")
+        get_trans()
 
-def new_entertainment(confirm_choice):
-    while confirm_choice != "Y":
-        selected_entertainment = random.choice(entertainment)
-        confirm_choice = input(f"Would you rather visit {selected_entertainment} instead? ")
-    else:
-        print(f"You have selected {selected_entertainment} as the destination for your trip. ")
 
-new_restaurant(confirm_choice)
 
-def new_transportation(confirm_choice):
-    while confirm_choice != "Y":
-        selected_transportation = random.choice(transportation)
-        confirm_choice = input(f"Would you rather visit {selected_transportation} instead? ")
-    else:
-        print(f"You have selected {selected_transportation} as the destination for your trip. ")
 
-new_transportation(confirm_choice)
 
+
+# confirm_choice = input("Please enter Y/N to confirm these travel options. ")
+
+# def new_destination(confirm_choice):
+#     while confirm_choice != "Y":
+#         selected_destination = random.choice(destinations)
+#         confirm_choice = input(f"Would you rather visit {selected_destination} instead? ")
+#     else:
+#         print(f"You have selected {selected_destination} as the destination for your trip. ")
+
+# new_destination(confirm_choice)
+
+# def new_restaurant(confirm_choice):
+#     while confirm_choice != "Y":
+#         selected_restaurant = random.choice(restaurants)
+#         confirm_choice = input(f"Would you rather go to {selected_restaurant} instead? ")
+#     else:
+#         print(f"You have selected {selected_restaurant} as the destination for your trip. ")
+
+# new_restaurant(confirm_choice)
+
+# def new_entertainment(confirm_choice):
+#     while confirm_choice != "Y":
+#         selected_entertainment = random.choice(entertainment)
+#         confirm_choice = input(f"Would you rather enjoy {selected_entertainment} instead? ")
+#     else:
+#         print(f"You have selected {selected_entertainment} as the destination for your trip. ")
+
+# new_restaurant(confirm_choice)
+
+# def new_transportation(confirm_choice):
+#     while confirm_choice != "Y":
+#         selected_transportation = random.choice(transportation)
+#         confirm_choice = input(f"Would you rather use {selected_transportation} instead? ")
+#     else:
+#         print(f"You have selected {selected_transportation} as the destination for your trip. ")
+
+# new_transportation(confirm_choice)
+
+
+
+
+
+# # Hard Mode
 # def edit_selection():
 #     print("Which option would you like to change? Destination, Restaurant, Entertainment, Transportation")
 #     change_response = input("""
@@ -119,21 +169,21 @@ new_transportation(confirm_choice)
 
 # def edit_transportation():
 #     get_transportation = selected_transportation()
-#     new_trans_response = input(f"Would you rathe use {get_transportation} instead? Y/N: ")
+#     new_trans_response = input(f"Would you rather use {get_transportation} instead? Y/N: ")
 #     if new_trans_response == "Y":
 #         print("Amazing!")
 #         get_summary()
 #     elif new_trans_response == "N":
 #         edit_transportation()
 
-# def get_summary():
-#     print(f""" Let's review your Day Trip:
-#     Destination: {get_destination}
-#     Restaurant: {get_restaurant}
-#     Entertainment: {get_entertainment}
-#     Transportation: {get_transportation}""")
-#     final_answer = input("Are you excited about this trip? Y/N : ")
-#     if final_answer == "Y":
-#         print("Congratulations! You are all set to enjoy your trip!")
-#     elif final_answer == "N":
-#         edit_selection()
+def get_summary():
+    print(f""" Let's review your Day Trip:
+    Destination: {selected_destination}
+    Restaurant: {selected_restaurant}
+    Entertainment: {selected_entertainment}
+    Transportation: {selected_transportation}""")
+    final_answer = input("Are you excited about this trip? Y/N : ")
+    if final_answer == "Y":
+        print("Congratulations! You are all set to enjoy your trip!")
+    elif final_answer == "N":
+        get_dest()
--- day_trip_ideas.py
+++ day_trip_ideas.py
@@ -17,7 +17,55 @@ day_trip = (f"""For your Day Trip we have randomly selected
 a. {selected_destination}
 b. {selected_restaurant}
 c. {selected_entertainment}
-d. {selected_transportation}
-""")
+d. {selected_transportation}""")
 print(day_trip)
+
 confirm_choice = input("Please enter Y/N to confirm this destination. ")
+
+def new_destination(confirm_choice):
+    while confirm_choice != "Y":
+        selected_destination = random.choice(destinations)
+        confirm_choice = input(f"Would you rather visit {selected_destination} instead? ")
+    else:
+        print(f"You have selected {selected_destination} as the destination for your trip.")
+
+new_destination(confirm_choice)
+
+def new_restaurant(confirm_choice):
+    while confirm_choice != "Y":
+        selected_restaurant = random.choice(restaurants)
+        confirm_choice = input(f"Would you rather go to {selected_restaurant} instead? ")
+    else:
+        print(f"You have selected {selected_restaurant} as the restaurant for your trip.")
+
+new_restaurant(confirm_choice)
+
+def new_entertainment(confirm_choice):
+    while confirm_choice != "Y":
+        selected_entertainment = random.choice(entertainment)
+        confirm_choice = input(f"Would you rather enjoy {selected_entertainment} instead? ")
+    else:
+        print(f"You have selected {selected_entertainment} as the entertainment for your trip. ")
+
+new_entertainment(confirm_choice)
+
+def new_transportation(confirm_choice):
+    while confirm_choice != "Y":
+        selected_transportation = random.choice(transportation)
+        confirm_choice = input(f"Would you rather use {selected_transportation} instead? ")
+    else:
+        print(f"You have selected {selected_transportation} as the transportation for your trip. ")
+
+new_transportation(confirm_choice)
+
+def get_review():
+    print(f""" Let's review your Day Trip:
+    Destination: {selected_destination}
+    Restaurant: {selected_restaurant}
+    Entertainment: {selected_entertainment}
+    Transportation: {selected_transportation}""")
+    final_answer = input("Are you excited about this trip? Y/N : ")
+    if final_answer == "Y":
+        print("Congratulations! You are all set to enjoy your trip!")
+    elif final_answer == "N":
+        new_destination()
